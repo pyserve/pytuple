59d00bc5992c4632cee6afc718dfc92208aadcc1
Anil Dhakal <aneildhakal21@gmail.com>
2025-07-19 13:01:00 -0400

    added static url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,14 @@
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter
-
 from accounts.urls import router as account_routers
 from accounts.views import GoogleLogin
 from call.urls import router as call_routers
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from lead.urls import router as lead_routers
 from machinelearning.urls import router as aiml_routers
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.registry.extend(account_routers.registry)
@@ -25,3 +26,5 @@ urlpatterns = [
     path("auth/token/", views.obtain_auth_token),
     path("", include(router.urls)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
